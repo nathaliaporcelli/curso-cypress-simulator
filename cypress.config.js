785d03d3c511222b8949c879497f2166e90ba5f7
nathalia <nathalia_porcelli@hotmail.com>
2025-09-21 17:34:15 -0300

    fix bug timed out

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ const cypressSplit = require('cypress-split')
 module.exports = defineConfig({
   viewportHeight: 1024,
   viewportWidth: 1700,
+  defaultCommandTimeout: 6000,
   e2e: {
     setupNodeEvents(on, config) {
       cypressSplit(on, config)
